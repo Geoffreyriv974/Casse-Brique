60f34acb3f19e61223af8f04bf29286a29a0aa5e
geoffrey riviere <119594717+Geoffreyriv974@users.noreply.github.com>
2024-03-08 17:55:48 +0100

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-score_file = "tableau_scores.csv"
-
-with open(score_file, 'r') as f:
-    reader = csv.DictReader(f, dialect=csv.QUOTE_ALL)
-
-    for value in reader:
-        print(value)
